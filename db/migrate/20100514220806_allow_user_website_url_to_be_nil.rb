3c905ddd6ccc3fb6f4853c80e54a1211ade416c8
Mr. Matt <system9@composition9.com>
2010-05-23 10:23:18 -0600

    removed old db migrations

--- db/migrate/20100514220806_allow_user_website_url_to_be_nil.rb
+++
@@ -1,9 +0,0 @@
-class AllowUserWebsiteUrlToBeNil < ActiveRecord::Migration
-  def self.up
-    change_column :users, :website_url, :string, :null => true
-  end
-
-  def self.down
-    change_column :users, :website_url, :string, :null => false
-  end
-end
